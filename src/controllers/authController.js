3beae22513a9eeb026f6301dc420bba361c68ecb
maustem <diego.maustem@gmail.com>
2025-02-20 17:11:35 -0300

    :sparkles: feat: Implementing validator of register

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,23 @@
+const { validationResult } = require("express-validator");
 const { registerSchema } = require("../utils/validators");
 
-exports.register = async (req, res) => {
-  res.status(201).json("Arrived");
-};
+const users = [];
+
+exports.register = [
+  registerSchema,
+
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ message: errors.array()[0].msg });
+    }
+
+    const newUser = req.body;
+    users.push(newUser);
+
+    res
+      .status(201)
+      .json({ message: "User registered successfully!", user: users });
+  },
+];
--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,27 +1,23 @@
-const express = require("express");
-const { body, validationResult } = require("express-validator");
-const app = express();
-
-app.use(express.json());
+const { body } = require("express-validator");
 
 const registerSchema = [
   body("name")
     .isString()
     .withMessage("Just letters.")
     .isLength({ min: 3 })
-    .withMessage("At least 3 characters are required of name.")
+    .withMessage("At least 3 characters are required for the name.")
     .notEmpty()
     .withMessage("The field name cannot be empty."),
   body("email")
     .notEmpty()
     .withMessage("The field email cannot be empty.")
     .isEmail()
-    .withMessage("Email invalid."),
+    .withMessage("Invalid email."),
   body("password")
     .notEmpty()
     .withMessage("The field password cannot be empty.")
     .isLength({ min: 6 })
-    .withMessage("At least 6 characters are required of password."),
+    .withMessage("At least 6 characters are required for the password."),
 ];
 
 module.exports = { registerSchema };
